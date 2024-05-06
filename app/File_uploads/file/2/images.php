f1c5e70b47119d95fe152e37cbb5af71718fbc64
NaNColor <5863kc1rgrd3@mail.ru>
2024-05-06 16:36:01 -0400

    added some changes before I end seance

--- app/File_uploads/file/2/images.php
+++ app/File_uploads/file/2/images.php
@@ -8,6 +8,13 @@
     <link rel=”stylesheet” href=”https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css” />
     <link rel="stylesheet" href="../static/style.css">
 </head>
+<?php 
+
+$directory = "upload";
+$images = glob($directory . "/*.jpg");
+$images = $images + glob($directory . "/*.png");
+
+?>
 <body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <div class="container nav_bar">
@@ -19,15 +26,19 @@
         </div>
     </div>
     <header>
-        <h1>Upload Your Favorite Image!</h1>
+        <h1>There are your images!</h1>
     </header>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h2> Only jpeg or png files!</h2>
-            </div>
-            <div class="col-12">
-        </div>
+    <div class="grid-container">
+
+        <?php
+
+        foreach($images as $image)
+        {
+            echo "<div class='border-item image-item'>";
+            echo "<img src='$image' alt='$image' width='400' height='400'>";
+            echo "</div>";
+        }
+        ?>        
     </div>
 
     <script type="text/javascript" src="js/button.js"></script>
