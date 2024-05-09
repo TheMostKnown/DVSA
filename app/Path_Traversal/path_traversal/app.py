bb6d99ed779a2d149d79090c20ac6bc757f62a2b
TNB86 <lyubog2@mail.ru>
2024-05-09 22:50:27 +0300

    Add path traversal tasks

---
+++ app/Path_Traversal/path_traversal/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, send_file, request
+
+app = Flask(__name__)
+
+flag_basic = "FLAG{Basic_Path_Traversal_Success}"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/get_basic')
+def get_basic():
+    filename = request.args.get('file')
+    if filename == 'secret.txt':
+        return flag_basic
+    else:
+        return "File not found"
+
+
+@app.route('/get_medium')
+def get_medium():
+    filename = request.args.get('file')
+    if filename.endswith('.txt'):
+        return send_file(filename)
+    else:
+        return "Invalid file type"
+
+
+@app.route('/get_advanced')
+def get_advanced():
+    filename = request.args.get('file')
+    if '../' in filename:
+        return "Invalid path"
+    try:
+        with open(f"templates/{filename}", 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        return "File not found"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
